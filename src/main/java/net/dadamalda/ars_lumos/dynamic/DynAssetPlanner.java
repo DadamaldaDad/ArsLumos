2b06340f42630fe7383dab15413fb52da3ac5a62
DadamaldaDad <dadamaldac@gmail.com>
2025-08-23 23:17:45 +0200

    Laid more groundwork for configs

--- src/main/java/net/dadamalda/ars_lumos/dynamic/DynAssetPlanner.java
+++ src/main/java/net/dadamalda/ars_lumos/dynamic/DynAssetPlanner.java
@@ -15,6 +15,9 @@ import javax.annotation.Nullable;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public final class DynAssetPlanner {
@@ -23,19 +26,22 @@ public final class DynAssetPlanner {
     public static final ResourceLocation EMPTY_TEXTURE = ResourceLocation.fromNamespaceAndPath("dynamic_asset_generator", "empty");
     public static final ResourceLocation EMISSIVE_PROPERTIES = ResourceLocation.parse("minecraft:optifine/emissive.properties");
 
+    public static Set<ResourceLocation> ASSETS = new HashSet<>();
+    public static Map<ResourceLocation, ResourceLocation> ASSET_MAP = new HashMap<>();
+
     public static void plan() {
         ASSET_CACHE.planSource(new PathAwareInputStreamSource() {
             @Override
             public @NotNull Set<ResourceLocation> getLocations(ResourceGenerationContext resourceGenerationContext) {
+                ASSETS.clear();
+                ASSETS.add(EMISSIVE_PROPERTIES);
+                ASSET_MAP.clear();
+
                 if(Config.enableTest) {
-                    return Set.of(
-                            EMISSIVE_PROPERTIES,
-                            ResourceLocation.parse("ars_nouveau:textures/block/blue_archwood_log_e.png"),
-                            ResourceLocation.parse("ars_nouveau:textures/block/blue_archwood_log_e.png.mcmeta")
-                    );
-                } else {
-                    return Set.of(EMISSIVE_PROPERTIES);
+                    addTexture(ResourceLocation.parse("ars_nouveau:textures/block/blue_archwood_log_e"), true);
                 }
+
+                return ASSETS;
             }
 
             @Override
@@ -43,13 +49,39 @@ public final class DynAssetPlanner {
             public IoSupplier<InputStream> get(ResourceLocation resourceLocation, ResourceGenerationContext resourceGenerationContext) {
                 if(resourceLocation.equals(EMISSIVE_PROPERTIES)) {
                     return () -> new ByteArrayInputStream("suffix.emissive=_e".getBytes(StandardCharsets.UTF_8));
-                } else if(resourceLocation.equals(ResourceLocation.parse("ars_nouveau:textures/block/blue_archwood_log_e.png"))) {
-                    return resourceGenerationContext.getResourceSource()
-                            .getResource(ResourceLocation.parse("ars_lumos:textures/block/blue_archwood_log_e.png"));
+                } else {
+                    return resourceGenerationContext.getResourceSource().getResource(ASSET_MAP.getOrDefault(resourceLocation, EMPTY_TEXTURE));
                 }
-                return resourceGenerationContext.getResourceSource()
-                        .getResource(ResourceLocation.parse("ars_lumos:textures/block/blue_archwood_log_e.png.mcmeta"));
             }
         });
+    }
+
+    private static void addAsset(ResourceLocation output, ResourceLocation input) {
+        ASSETS.add(output);
+        ASSET_MAP.put(output, input);
+    }
+
+    private static void addTexture(ResourceLocation location, String variant, boolean hasMcmeta) {
+        ResourceLocation input = ResourceLocation.fromNamespaceAndPath(Ars_lumos.MODID,
+                "textures/configurable/"+variant+"/"+location.getNamespace()+"/"+location.getPath());
+
+        addAsset(ResourceLocation.fromNamespaceAndPath(location.getNamespace(), location.getPath()+".png"),
+                ResourceLocation.fromNamespaceAndPath(input.getNamespace(), input.getPath()+".png"));
+        if(hasMcmeta) {
+            addAsset(ResourceLocation.fromNamespaceAndPath(location.getNamespace(), location.getPath()+".png.mcmeta"),
+                    ResourceLocation.fromNamespaceAndPath(input.getNamespace(), input.getPath()+".png.mcmeta"));
+        }
+    }
+
+    private static void addTexture(ResourceLocation location, String variant) {
+        addTexture(location, variant, false);
+    }
+
+    private static void addTexture(ResourceLocation location) {
+        addTexture(location, "default", false);
+    }
+
+    private static void addTexture(ResourceLocation location, boolean hasMcmeta) {
+        addTexture(location, "default", hasMcmeta);
     }
 }
